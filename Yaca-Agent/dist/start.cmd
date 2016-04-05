3aa18925d5b1cba8c28c1c069ba9e7d69b33bc38
MarkusSprunck <markus.sprunck@mnet-online.de>
2016-04-05 23:28:30 +0200

    Improve starting of demo application and update build

--- Yaca-Agent/dist/start.cmd
+++ Yaca-Agent/dist/start.cmd
@@ -1,11 +1,9 @@
-echo YACA-AGENT - STATIC AND DYNAMIC ANALYSIS
+@echo off 
  
-SET JDK_BIN_PATH="C:/Program Files/Java/jdk1.8.0_77/bin/java.exe"
+set JAVA_HOME=C:/Program Files/Java/jdk1.8.0_77
+set PATH="%JAVA_HOME%\bin";%PATH%
  
-echo Start yaca agent test client.
-start %JDK_BIN_PATH% -Xint  -jar ".\YacaAgentTestClient.jar"
-echo.
- 
-echo Start yaca agent.
-%JDK_BIN_PATH% -jar ".\YacaAgent.jar"
+cmd /c "start /min  java -Xint  -jar YacaAgentTestClient.jar"
+cmd /c "start /min  chrome http://localhost:33333/monitor"
+java -jar YacaAgent.jar"
  
--- Yaca-Agent/dist/start.sh
+++ Yaca-Agent/dist/start.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 
-echo "YACA-AGENT - STATIC AND DYNAMIC ANALYSIS"
-
 JAVA_RUN_TIME=/Library/Java/JavaVirtualMachines/jdk1.8.0_77.jdk/Contents/Home/bin/java
-
 JAR1=/Users/markus/git/yaca-monitor/Yaca-Agent/dist/YacaAgentTestClient.jar
-if [ -f "$JAR1" ]; then
-  echo "Start yaca agent test client."
-  $JAVA_RUN_TIME -Xint -jar "$JAR1" &
-else
-  echo "File YacaAgentTestClient.jar doesn't exist."
-  exit 1
-fi
-
-echo "Start Yaca Agent."
-
 JAR2=/Users/markus/git/yaca-monitor/Yaca-Agent/dist/YacaAgent.jar
-$JAVA_RUN_TIME  -jar "$JAR2" &
 
+$JAVA_RUN_TIME -Xint -jar "$JAR1" &
+$JAVA_RUN_TIME  -jar "$JAR2" & 
+
+sleep 3
 /usr/bin/open -a "/Applications/Google Chrome.app" 'http://localhost:33333/monitor' 
+
